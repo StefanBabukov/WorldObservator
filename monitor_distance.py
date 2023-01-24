7477681c8afe333e17d38ce4f755fd9a65251c14
stefanbabukov <stefanbabukov98@gmail.com>
2023-01-24 16:33:15 +0000

    change

--- monitor_distance.py
+++ monitor_distance.py
@@ -14,6 +14,8 @@ GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 GPIO.setup(led, GPIO.OUT)
 
+distance = 0
+
 def get_distance():
      
     #GPIO pins
@@ -32,69 +34,30 @@ def get_distance():
 
     distance = (stop_time - start_time) * 34300 / 2
     return distance
+    
+def alert_user():
+    global distance
+    while True:
+        if distance > 100:
+            continue
+        blink_frequency = distance / 100
+        GPIO.output(led, True)
+        time.sleep(blink_frequency)
+        GPIO.output(led, False)
+        time.sleep(blink_frequency)
 
 def output_distance():
-        while True:
-            distance = get_distance()
-            print("Distance: ", distance, " cm")
-            if distance > 500:
-                continue
-            blink_frequency = distance / 100
-            GPIO.output(led, True)
-            time.sleep(blink_frequency)
-            GPIO.output(led, False)
-            time.sleep(blink_frequency)
+    global distance
+    while True:
+        distance = get_distance()
+        print("Distance: ", distance, " cm")
 
 try:
     output_thread = threading.Thread(target=output_distance)
+    output_thread = threading.Thread(target=alert_user)
+
     output_thread.start()
     output_thread.join()
 except KeyboardInterrupt:
     print("Measurement stopped by User")
     GPIO.cleanup()
-
-
-# def distance():
-#     # set Trigger to HIGH
-#     GPIO.output(GPIO_TRIGGER, True)
- 
-#     # set Trigger after 0.01ms to LOW
-#     time.sleep(0.00001)
-#     GPIO.output(GPIO_TRIGGER, False)
- 
-#     StartTime = time.time()
-#     StopTime = time.time()
- 
-#     # save StartTime
-#     while GPIO.input(GPIO_ECHO) == 0:
-#         StartTime = time.time()
- 
-#     # save time of arrival
-#     while GPIO.input(GPIO_ECHO) == 1:
-#         StopTime = time.time()
- 
-#     # time difference between start and arrival
-#     TimeElapsed = StopTime - StartTime
-#     # multiply with the sonic speed (34300 cm/s)
-#     # and divide by 2, because there and back
-#     distance = (TimeElapsed * 34300) / 2
- 
-#     return distance
-
-
-# print('starting to measure')
-# try:
-#     while True:
-#         dist = distance()
-#         print ("Measured Distance = %.1f cm" % dist)
-        
-#         if dist > 500:
-#             continue
-#         sleep_time = dist/100
-#         GPIO.output(led, True)
-#         time.sleep(sleep_time/2)
-#         GPIO.output(led, False)
-#         time.sleep(sleep_time/2)
-# except KeyboardInterrupt:
-#     print("Measurement stopped by User")
-#     GPIO.cleanup()
