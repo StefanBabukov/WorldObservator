69b7a496daf54ca900bc1659cdcef76d9cbb1383
stefanbabukov <stefanbabukov98@gmail.com>
2023-01-24 13:56:32 +0000

    revert

--- monitor_distance.py
+++ monitor_distance.py
@@ -6,20 +6,19 @@ GPIO_TRIGGER = 7
 GPIO_ECHO = 11
 led = 13
 
+# following the board pin numbering 
+GPIO.setmode(GPIO.BOARD)
 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(led, GPIO.OUT)
 
 def get_distance():
-    # following the board pin numbering 
-    GPIO.setmode(GPIO.BOARD)
-
-    #set GPIO direction (IN / OUT)
-    GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-    GPIO.setup(GPIO_ECHO, GPIO.IN)
-    GPIO.setup(led, GPIO.OUT)
-
+     
     #GPIO pins
     GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(0.05)
+    time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
 
     start_time = time.time()
